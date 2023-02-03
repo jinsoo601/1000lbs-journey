fc4cf31298f931e6a40e30ce4a4cac9ba7ef592e
Jin Soo Lim <jinsoo601@gmail.com>
2023-02-03 13:48:50 -0800

    add missing type

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -3,6 +3,7 @@ import type { GetServerSideProps } from "next/types";
 import { getServerSession } from "next-auth/next";
 import { useSession, signOut } from "next-auth/react";
 import { authOptions } from "./api/auth/[...nextauth]";
+import { Session } from "next-auth";
 
 export default function Timeline() {
   const { data: session, status } = useSession();
@@ -16,7 +17,9 @@ export default function Timeline() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+export const getServerSideProps: GetServerSideProps<{
+  session: Session;
+}> = async (context) => {
   const session = await getServerSession(context.req, context.res, authOptions);
 
   if (!session) {
